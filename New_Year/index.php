cbf87eebf663ac3eca75a4d49fd7ff4e4de64fb7
saurabhshukla01 <ss7838094755@gmail.com>
2019-12-16 21:18:34 +0530

    added year

---
+++ New_Year/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html lang="en_US">
+<head>
+	<meta charset="UTF-8">
+	<title>Happy New Year</title>
+	<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+	<style>
+		body {font-family: 'Roboto', sans-serif;}
+		form {}
+		form label {font-size:12px; color:#fff; padding:10px;}
+		form input {font-size:12px; padding:10px; border:1px #ccc solid; background:#FFF; border-radius:5px;}
+	</style>
+</head>
+<body background="https://d1otfi4uhdq3fm.cloudfront.net/content/uploads/2017/12/26074018/Singapore-fireworks.jpg" style="background-size:cover;">
+
+	<form action="New_Year.php" method="get">
+    	<label for="name">Enter Text</label>
+        <input type="text" name="name" id="name">
+        
+        <input type="submit" name="submit" value="Let's Go">
+    </form>
+	
+</body>
+</html>
